fb31d974ec609a19e629a35ee4983b15eae5d402
htetkohmue <htetkohmue786@gmail.com>
2022-05-12 08:13:40 +0630

    start react project

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form">
+          <div className="form-body">
+              <div className="username">
+                  <label className="form__label" for="firstName">First Name </label>
+                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
+              </div>
+              <div className="lastname">
+                  <label className="form__label" for="lastName">Last Name </label>
+                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
+              </div>
+              <div className="email">
+                  <label className="form__label" for="email">Email </label>
+                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+              </div>
+              <div className="password">
+                  <label className="form__label" for="password">Password </label>
+                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
+              </div>
+              <div className="confirm-password">
+                  <label className="form__label" for="confirmPassword">Confirm Password </label>
+                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
+              </div>
+          </div>
+          <div class="footer">
+              <button type="submit" class="btn">Register</button>
+          </div>
+      </div>      
+      <header className="App-header">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
